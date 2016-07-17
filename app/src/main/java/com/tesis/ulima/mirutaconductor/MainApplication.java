16a284715cba1db548cbb118576235ae17ad65e4
Christian <challme28@gmail.com>
2016-07-17 16:40:09 -0500

    Multidex aplicado

--- app/src/main/java/com/tesis/ulima/mirutaconductor/MainApplication.java
+++ app/src/main/java/com/tesis/ulima/mirutaconductor/MainApplication.java
@@ -1,13 +1,16 @@
 package com.tesis.ulima.mirutaconductor;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+import android.support.multidex.MultiDexApplication;
 
 import com.parse.Parse;
 
 /**
  * Created by Christian on 5/14/2016.
  */
-public class MainApplication extends Application {
+public class MainApplication extends MultiDexApplication {
 
     @Override
     public void onCreate() {
@@ -16,5 +19,11 @@ public class MainApplication extends Application {
                 .applicationId("449e9382040418fff7bd75dfae5c6a7260abbc69")
                 .server("http://miruta.frikicorp.com/parse/")
                 .build());
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
     }
 }
